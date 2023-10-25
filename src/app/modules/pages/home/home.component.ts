17b68c20c47d499faeb47a119ba5f8d86f42e196
Milad Raeisi <miladsoft@yahoo.com>
2023-10-25 08:50:12 +0400

    Update home page boxes

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -7,9 +7,16 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgClass, NgFor, TitleCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { SetupService } from 'app/core/services/setup.service';
+import { OmanOnlineCardComponent } from '@omanonline/components/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector: 'home',
@@ -23,19 +30,21 @@ import { SetupService } from 'app/core/services/setup.service';
         RouterLink,
         MatExpansionModule,
         NgFor,
+        CommonModule,
+        OmanOnlineCardComponent,
+        MatButtonModule, NgClass, MatMenuModule, MatProgressBarModule, TextFieldModule, MatDividerModule, MatTooltipModule, TitleCasePipe
     ],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-     _businessCategories: any[];
+    _businessCategories: any[];
 
-     _business: any[];
+    _business: any[];
 
     constructor(private setup: SetupService) {}
 
     async ngOnInit(): Promise<void> {
-        this._businessCategories= await this.setup.getCategories();
-        this._business= await this.setup.getBusinesses();
-
+        this._businessCategories = await this.setup.getCategories();
+        this._business = await this.setup.getBusinesses();
     }
 
     ngOnDestroy(): void {}
